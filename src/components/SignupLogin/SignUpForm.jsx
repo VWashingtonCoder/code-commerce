adc45b8a3bbd6df91e627e3b8efab25529a64026
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-02-21 15:25:35 -0700

    Finish structure and logic

--- src/components/SignupLogin/SignUpForm.jsx
+++ src/components/SignupLogin/SignUpForm.jsx
@@ -1,81 +1,123 @@
 import { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai"
+import { initSignUpForm, initSignUpErrors, pwInputsSignUp, nameInputsSignUp } from "./data";
 
-const SignUpForm = () => {
-  const initStates = {
-    email: "",
-    password: "",
-    confirm: "",
-    firstName: "",
-    surname: "",
-    postCode: "",
-    showPassword: false,
-    showConfirmPW: false,
-    passwordMatch: true,
-  };
-  const [formValues, setFormValues] = useState(initStates);
-  const { password, showPassword, showConfirmPW, passwordMatch } = formValues;
-  const pwInputs = [
-    {
-      key: "pw",
-      text: "Create Password *",
-      state: "showPassword",
-      name: "password",
-      bottom: "Password must be 8-20 characters",
-    },
-    {
-      key: "con",
-      text: "Confirm Password *",
-      state: "showConfirmPW",
-      name: "confirm",
-      bottom: "Passwords don't match",
-    },
-  ];
-  const nameInputs = [
-    { key: "first", label: "First Name *", name: "firstName" },
-    { key: "sur", label: "Surname *", name: "surname" },
-  ];
+const SignUpForm = (props) => {
+  const [formValues, setFormValues] = useState(initSignUpForm);
+  const [errors, setErrors] = useState(initSignUpErrors);
+  const { showPW, showCPW, accounts, hide, add } = props
+  const { email, password, postCode } = formValues;
+  
+function validatePassword(pass) {
+    const regExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
+    if(pass.length < 8 || pass.length > 20 || !regExp.test(pass)) 
+        return false;
+    else return true
+}
 
-  const checkPassword = (value) => {
-    if (value === password || (value !== password && passwordMatch === true))
-      setFormValues({ ...formValues, passwordMatch: !passwordMatch });
-  };
+function containsNumbers(str) {
+    return /\d/.test(str);
+}
+
+function containsOnlyNumbers(str) {
+    return /^\d+$/.test(str);
+}
+
+const validateValue = (e) => {
+    let { name, value } = e.target;
+    setErrors(prev => {
+        const errorObj = { ...prev, [name]: "" };
+
+        switch(name) {
+            case "email":
+                const emailObj = accounts.find((account) => account.email === value);
+                if (!value || !value.includes('@') || !value.includes('.'))
+                    errorObj[name] = "Please enter a valid email.";
+                else if (emailObj)
+                    errorObj[name] = "Email is already in use. Log in instead."
+                break;
+            case "password":
+                if(!value) 
+                    errorObj[name] = "Please enter a password.";
+                else if(!validatePassword(value)) 
+                    errorObj[name] = "Password must be 8-20 characters including at least 1 of each: lowercase letter, uppercase letter, number, and symbols(!@#$%^&*).";
+                break;
+            case "confirm":
+                if (!value)
+                    errorObj[name] = "Please confirm your password."
+                else if (password !== value)
+                    errorObj[name] = "Your passwords aren't matching. Please try again." 
+                break;
+            case "firstName":
+                if (!value)
+                    errorObj[name] = "Please enter your first name."
+                else if(containsNumbers(value))
+                    errorObj[name] = "You cannot use numbers in your first name."
+                break;
+            case "surname":
+                if (!value)
+                    errorObj[name] = "Please enter your surname."
+                else if(containsNumbers(value))
+                    errorObj[name] = "You cannot use numbers in your surname."
+                break;
+            case "postCode":
+                if (!value)
+                    errorObj[name] = "Please enter your postcode." 
+                if (!containsOnlyNumbers(value))
+                    errorObj[name] = "You cannot use letters in your postcode."
+                break;
+            default:
+                break;
+        }
+
+        return errorObj;
+    })
+  }
 
   const changeValues = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
-    if (name === "confirm") checkPassword(value);
   };
 
-  const toggleHide = (e) => {
+  const resetForm = () => {
+    setFormValues(initSignUpForm);
+  }
+
+  const submit = (e) => {
     e.preventDefault();
-    e.target.value === "password"
-      ? setFormValues({ ...formValues, showPassword: !showPassword })
-      : setFormValues({ ...formValues, showConfirmPW: !showConfirmPW });
-  };
+    add(formValues);
+    resetForm();
+  }
+  
 
   return (
-    <>
-      <form id="SignUpForm">
+      <form id="SignUpForm" onSubmit={submit}>
         <div className="sign-up form-input email">
           Your Email Address *
           <input
             type="email"
             name="email"
+            value={email}
             onChange={changeValues}
+            onBlur={validateValue}
             required
             autoComplete="off"
           />
+          {errors.email}
         </div>
-        {pwInputs.map((input) => {
-          const { key, text, state, name, bottom } = input;
+        {pwInputsSignUp.map((input) => {
+          const { key, text, name } = input;
+          const state = name === "password" ? showPW : showCPW; 
 
           return (
             <div key={key} className={`sign-up form-input ${name}`}>
               {text}
               <input
-                type={formValues[state] ? "text" : "password"}
+                type={state ? "text" : "password"}
                 name={name}
+                value={formValues[name]}
                 onChange={changeValues}
+                onBlur={validateValue}
                 minLength="8"
                 maxLength="20"
                 required
@@ -83,44 +125,48 @@ const SignUpForm = () => {
               <button
                 className={`pw-btn ${name}`}
                 value={name}
-                onClick={toggleHide}
+                onClick={hide}
               >
-                Show Password
+                {formValues[state] 
+                    ? <AiFillEyeInvisible className="eye-icon" /> 
+                    : <AiFillEye className="eye-icon" />
+                }
               </button>
-              {key === "pw" && <p>{bottom}</p>}
-              {key === "con" && !passwordMatch && <p>{bottom}</p>}
+              {errors[name]}
             </div>
           );
         })}
-        {nameInputs.map((input) => {
+        {nameInputsSignUp.map((input) => {
           const { key, label, name } = input;
 
           return (
             <div key={key} className={`sign-up form-input ${name}`}>
               {label}
-              <input type="text" name={name} onChange={changeValues} required />
+              <input 
+                type="text" 
+                name={name} 
+                value={formValues[name]}
+                onBlur={validateValue} 
+                onChange={changeValues} 
+                required 
+              />
+              {errors[name]}
             </div>
           );
         })}
         <div className="sign-up form-input postCode">
           Postcode
-          <input type="text" name="postCode" onChange={changeValues} />
+          <input 
+            type="text" 
+            name="postCode" 
+            value={postCode}
+            onChange={changeValues} 
+            onBlur={validateValue} 
+          />
+          {errors.postCode}
         </div>
         <input type="submit" value="Save" />
       </form>
-
-      <div className="operator">OR</div>
-
-      <div className="facebook-btn">
-        <p>Icon</p>
-        <p>Sign Up With Facebook</p>
-      </div>
-
-      <footer>
-        <p>Privacy Policy and Cookies</p>
-        <p>Terms of Sale and Use</p>
-      </footer>
-    </>
   );
 };
 
--- src/components/SignupLogin/SignupLogin.jsx
+++ src/components/SignupLogin/SignupLogin.jsx
@@ -3,28 +3,75 @@ import "./SignupLogin.css";
 import SignUpForm from "./SignUpForm";
 
 const SignupLogin = () => {
-    const [signLog, setSignLog] = useState("sign-up");
-    
-    const toggleSignLog = (e) => {
-        setSignLog(e.target.value);
+  const [signLog, setSignLog] = useState("sign-up");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPW, setShowConfirmPW] = useState(false);
+  const [accounts, setAccounts] = useState([]);
+
+  const toggleSignLog = (e) => {
+    setSignLog(e.target.value);
+  };
+
+  const toggleHide = (e) => {
+    e.preventDefault();
+    e.target.value === "password"
+      ? setShowPassword(!showPassword)
+      : setShowConfirmPW(!showConfirmPW);
+  };
+
+  const addToAccounts = (values) => {
+    const { 
+        email, 
+        password, 
+        firstName, 
+        surname, 
+        postCode 
+    } = values;
+    const accountInfo = {
+        key: accounts.length + 1,
+        email: email,
+        password: password,
+        name: `${firstName} ${surname}`,
+        zip: postCode
     }
+    setAccounts([...accounts, accountInfo]);
+  }
 
-    return (
-        <div id="SignupLogin">
-            <h1>SignupLogin</h1>
-            <div className="top-bar">
-                <div className="close-x header-md">X</div>
-                <div className="sign-log-inputs" onChange={toggleSignLog}>
-                    <input type="radio" value="sign-up" name="signLog" defaultChecked/> Create Account
-                    <input type="radio" value="login" name="signLog" /> Log In
-                </div>
-            </div>
 
-            {
-                signLog === "sign-up" ? <SignUpForm/> : null
-            }
+  return (
+    <div id="SignupLogin">
+      <h1>SignupLogin</h1>
+      <div className="top-bar">
+        <div className="close-x header-md">X</div>
+        <div className="sign-log-inputs" onChange={toggleSignLog}>
+          <input type="radio" value="sign-up" name="signLog" defaultChecked />{" "}
+          Create Account
+          <input type="radio" value="login" name="signLog" /> Log In
         </div>
-    );
-}
+      </div>
+
+      {signLog === "sign-up" ? (
+        <SignUpForm
+          showPW={showPassword}
+          showCPW={showConfirmPW}
+          accounts={accounts}
+          hide={toggleHide}
+          add = {addToAccounts}
+        />
+      ) : null}
+      <div className="operator">OR</div>
+
+      <div className="facebook-btn">
+        <span>Icon</span>
+        Sign {signLog==="sign-up" ? "Up" : "In"} With Facebook
+      </div>
+
+      <footer>
+        <p>Privacy Policy and Cookies</p>
+        <p>Terms of Sale and Use</p>
+      </footer>
+    </div>
+  );
+};
 
 export default SignupLogin;
