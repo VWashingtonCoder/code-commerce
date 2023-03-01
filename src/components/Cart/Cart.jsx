2a1cd72caecdd330540d26cce856f5baebaae7fe
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-02-28 17:15:47 -0700

    Remove unnecessary comments

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -16,39 +16,6 @@ const Cart = () => {
         <Table />
       </div>
       <Summary sub={subtotal} total={total} />
-{/* 
-
-      <div className="cart-summary">
-        <div className="summary-title">Summary</div>
-        <div className="promo">
-          <label htmlFor="promo-code">Do you have a promo code?</label>
-          <input type="text" name="promo-code" placeholder="Code" />
-          <button className="promo-apply-btn">Apply</button>
-        </div>
-        <div className="cart-totals">
-          <div className="cart-subtotal totals-container">
-            <p className="total-title">Cart Subtotal:</p>
-            <p className="total-num">
-              {subtotal === 0 ? "-" : `$${subtotal.toFixed(2)}`}
-            </p>
-          </div>
-          <div className="cart-shipping totals-container">
-            <p className="total-title">Shipping & Handling:</p>
-            <p className="total-num">-</p>
-          </div>
-          <div className="cart-shipping totals-container">
-            <p className="total-title">Discount:</p>
-            <p className="total-num">-</p>
-          </div>
-          <div className="cart-total totals-container">
-            <p className="total-title">Cart Total:</p>
-            <p className="total-num">
-              {total === 0 ? "-" : `$${total.toFixed(2)}`}
-            </p>
-          </div>
-        </div>
-        <button className="checkout-btn">Checkout</button>
-      </div> */}
     </div>
   );
 };
