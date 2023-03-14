aba08b4409b0003d37e070da00133af7ce311d8e
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-03-14 11:24:51 -0600

    Update styles and logic for statusbar

--- src/components/StatusBar/StatusBar.jsx
+++ src/components/StatusBar/StatusBar.jsx
@@ -3,35 +3,29 @@ import { TbTruckDelivery } from "react-icons/tb";
 import { MdPayment } from "react-icons/md";
 import { GiConfirmed } from "react-icons/gi";
 import { useState } from "react";
+import "./StatusBar.css"
 
 const StatusBar = ({page}) => {
-    const [barProgress, setBarProgress] = useState({ 
-        ship: false, 
-        pay: false,  
-    });
+    const [barProgress, setBarProgress] = useState(
+        { ship: false, pay: false }
+    );
     const { ship, pay } = barProgress;
 
 
     return (
-        <div id="StatusBar">
-            <div className="logo-circle cart">
+        <div id="StatusBar" className="flex-align-center">
+            <div className="logo-circle done">
                 <AiFillCheckCircle />
             </div>
-            <div className="logo-bar" style={{backgroundColor: "grey", height: "20px", width: "50px" }}></div>
-            <div className="logo-circle ship">
-                {barProgress.ship 
-                    ? (<TbTruckDelivery />)
-                    : (<AiFillCheckCircle />)
-                }
+            <div className="logo-bar done"></div>
+            <div className={`logo-circle ${ship && "done"}`}>
+                {ship ? (<AiFillCheckCircle />) : (<TbTruckDelivery />)}
             </div>
-            <div className="logo-bar" style={{backgroundColor: "grey", height: "20px", width: "50px" }}></div>
-            <div className="logo-circle pay">
-                {barProgress.pay 
-                    ? (<MdPayment />)
-                    : (<AiFillCheckCircle />)
-                }
+            <div className={`logo-bar ${ship && "done"}`}></div>
+            <div className={`logo-circle ${pay && "done"}`}>
+                {pay ? (<AiFillCheckCircle />) : (<MdPayment />)}
             </div>
-            <div className="logo-bar" style={{backgroundColor: "grey", height: "20px", width: "50px" }}></div>
+            <div className={`logo-bar ${pay && "done"}`}></div>
             <div className="logo-circle confirm">
                 <GiConfirmed />
             </div>
