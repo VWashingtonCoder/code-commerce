15a63d514e8430b713fd00e9ce93de6c887b530b
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-03-24 17:04:03 -0600

    Set up pay form and error states

--- src/pages/P4.Payment/Payment.jsx
+++ src/pages/P4.Payment/Payment.jsx
@@ -1,24 +1,79 @@
 import "./Payment.css";
 import { monthOptions, yearOptions } from "../../data";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { useState } from "react";
+
+const initCardForm = {
+  cardName: "",
+  cardNum: "",
+  expMonth: "",
+  expYear: "",
+  cvv: ""
+}
+
+const errorKeys = ["cardName", "cardNum", "expMonth", "expYear", "cvv"];
 
 const Payment = () => {
+  const [cardValues, setCardValues] = useState(initCardForm);
+  const [errors, setErrors] = useState({});
+
+  function validateCardFormValues(name, val) {
+    let valid = true;
+    let error = "";
+
+    switch(name) {
+      case "cardName":
+        break;
+      default: 
+        break;
+    }
+
+    return { valid: valid, error: error };
+  } 
+
+  const updateCardValues = (e) => {
+    const { id, value } = e.target;
+    const { valid, error } = validateCardFormValues(id, value);
+    if (valid) setCardValues({ ...cardValues, [id]: value });
+    setErrors({ ...errors, [id]: error });
+  }
 
   return (
     <div id="Payment">
       <h2 className="underline-border">Payment Information</h2>
+      {errors && (
+        <div className="errors-box">
+          {errorKeys.map(key => (
+            <p key={key} className="error-text">{errors[key]}</p>
+          ))}
+        </div>
+      )}
       <div className="pay-inputs-container">
         <div className="pay-row name">
           <label htmlFor="cardName">Cardholder Name</label>
-          <input type="text" id="cardName" />
+          <input 
+            type="text" 
+            id="cardName" 
+            value={cardValues.cardName} 
+            onChange={updateCardValues} 
+          />
         </div>
         <div className="pay-row num">
           <label htmlFor="cardNum">Card Number</label>
-          <input type="text" id="cardNum" />
+          <input 
+            type="text" 
+            id="cardNum"
+            value={cardValues.cardNum} 
+            onChange={updateCardValues}  
+          />
         </div>
         <div className="pay-row exp">
           <span className="exp-label">Exp Date</span>
-          <select id="expMonth">
+          <select 
+            id="expMonth"
+            value={cardValues.expMonth} 
+            onChange={updateCardValues} 
+          >
             {monthOptions.map((month) => {
               const { key, text, value } = month;
               return (
@@ -28,10 +83,14 @@ const Payment = () => {
               );
             })}
           </select>
-          <select id="expYear">
+          <select 
+            id="expYear"
+            value={cardValues.expYear} 
+            onChange={updateCardValues} 
+          >
             <option value="">Year</option>
             {yearOptions().map((year) => (
-              <option key={year} value={Number(year)}>
+              <option key={year} value={year}>
                 {year}
               </option>
             ))}
@@ -39,7 +98,12 @@ const Payment = () => {
         </div>
         <div className="pay-row cvv flex-align-center">
           <label htmlFor="cvv">CVV</label>
-          <input type="text" id="cvv" />
+          <input 
+            type="text" 
+            id="cvv"
+            value={cardValues.cvv} 
+            onChange={updateCardValues}  
+          />
           <AiOutlineQuestionCircle className="question-circle" />
         </div>
       </div>
