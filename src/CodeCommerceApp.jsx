269ea17c2cb8189be8cee1a61f6e5800a7b5f746
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-03-27 16:08:32 -0600

    Update cart and ship form for validation logic

--- src/CodeCommerceApp.jsx
+++ src/CodeCommerceApp.jsx
@@ -4,12 +4,9 @@ import {
   pageKeys,
   initBag,
   initTotals,
-  initShipFormValues,
-  initShipFormErrors,
   initBarProgress,
   shipMethods
 } from "./data-helpers/data";
-import { validateValues } from "./data-helpers/validation";
 import SignUpLogin from "./pages/P1.SignupLogin/SignupLogin";
 import Cart from "./pages/P2.Cart/Cart";
 import Shipping from "./pages/P3.Shipping/Shipping";
@@ -30,18 +27,16 @@ const testData = {
     ship: true,
     pay: false
   },
-  shipForm: {
-    addressTitle: "Home",
-    name: "Andre Blaze",
-    street: "88 Ex St",
-    zip: "81457",
-    country: "USA",
-    city: "Fountain",
-    state: "CO",
-    cellCode: "719",
-    cellNum: "8504",
-    telCode: "789",
-    telNum: "5486"
+  shipInfo: {
+    addressData: {
+      name: "Andre Blaze",
+      street: "88 Ex St",
+      zip: "81457",
+      country: "USA",
+      city: "Fountain",
+      state: "CO",
+    },
+    methodData: { method: "standard", info: "3-4 Days info" }
   },
   totals: { 
     items: { DS: 21.5, MHA: 27.25, NAR: 24 },
@@ -54,25 +49,28 @@ const testData = {
 
 
 const CodeCommerceApp = () => {
-  const [page, setPage] = useState(pageKeys[0]);
-  const [activeAccount, setActiveAccount] = useState(testData.account);
-  const [bag, setBag] = useState(initBag);
+  //Global
+  const [page, setPage] = useState(pageKeys[2]); // init: pageKeys[0] 
+  const [barProgress, setBarProgress] = useState(testData.barProgress); // init: initBarProgress
+  // SignUpLogin
+  const [activeAccount, setActiveAccount] = useState(testData.account); // init: {}
+  // Summary
   const [disabled, setDisabled] = useState(true); // init: false
-  const [totals, setTotals] = useState(testData.totals);
-  const [barProgress, setBarProgress] = useState(testData.barProgress);
-  const [shipFormValues, setShipFormValues] = useState(testData.shipForm);
-  const [shipFormErrors, setShipFormErrors] = useState(initShipFormErrors);
-  const [shipMethod, setShipMethod] = useState(shipMethods[0]);
-  const { items, subtotal, shipCost, discount, total } = totals;
+  const [totals, setTotals] = useState(testData.totals); // init: initTotals
+  const { 
+      items, 
+      subtotal, 
+      shipCost, 
+      discount, 
+      total 
+    } = totals;
+  // Cart
+  const [bag, setBag] = useState(initBag);
+  // Shipping
+  const [shipInfo, setShipInfo] = useState(testData.shipInfo); // init: {}
   const [payCard, setPayCard] = useState({});
 
-  const resetShipState = () => {
-    setTotals(initTotals);
-    setBarProgress(initBarProgress);
-    setShipFormValues(initShipFormValues);
-    setShipMethod("standard");
-    setDisabled(false);
-  };
+  
 
   const changePage = () => {
     const pageIdx = pageKeys.findIndex((key) => key === page);
@@ -84,23 +82,16 @@ const CodeCommerceApp = () => {
     setPage(pageKeys[pageIdx - 1]);
 
     if (page === "ship") {
-      resetShipState();
+      setDisabled(false);
+      setTotals({ ...totals, discount: 0, total: total + discount });
     }
   };
 
-  const checkFullForm = () => {
-    if (
-      (!Object.values(shipFormValues).includes("") && disabled === true) ||
-      (Object.values(shipFormValues).includes("") && disabled === false)
-    )
-      setDisabled(!disabled);
-  };
-
   const checkout = () => {
-    if (page === "cart"){
+    if (page === "cart") {
       setTotals({
         ...totals,
-        discount: 4.5,
+        discount: discount === 0 ? 4.5 : discount,
         total: total - 4.5,
       });
     } else if (page === "ship") {
@@ -131,54 +122,32 @@ const CodeCommerceApp = () => {
     setPayCard(cardData);
   }
 
-  const updateTotals = (itemTotals) => {
+  const updateCartTotals = (itemTotals) => {
     let sub = 0;
     const itemTotalsArr = Object.values(itemTotals);
     itemTotalsArr.forEach((total) => (sub = sub + total));
     setTotals({
+      ...totals,
       items: itemTotals,
       subtotal: sub,
       total: sub,
     });
   };
 
-  const updateShipMethod = (e) => {
-    const method = e.target.value;
-    const methodInfo = shipMethods.find(type => type.key === method);
+  const updateShipInfo = (shipInfo) => {
+    setShipInfo(shipInfo);
+  }
 
-
-    if (method === "standard" && shipCost === 5) {
-      setTotals({
-        ...totals,
-        shipCost: 0,
-        total: total - 5,
-      });
-    } else if (method === "express") {
-      setTotals({
-        ...totals,
-        shipCost: 5,
-        total: total + 5,
-      });
-    }
-    setShipMethod(methodInfo);
-  };
-
-  const updateShipFormValues = (e) => {
-    const { name, value } = e.target;
-    const { valid, error } = validateValues(name, value);
-
-    if (valid) {
-      setShipFormValues({
-        ...shipFormValues,
-        [name]: value,
-      });
-    }
-
-    setShipFormErrors({
-      ...shipFormErrors,
-      [name]: error,
+  const updateShipTotals = (cost) => {
+    setTotals({ 
+      ...totals, 
+      shipCost: cost,
+      total: cost < shipCost ? total - shipCost : total + cost
     });
-  };
+  }
+  
+ 
+  
 
   return (
     <div id="CodeCommerceApp">
@@ -198,7 +167,7 @@ const CodeCommerceApp = () => {
               updateBag={updateBag}
               updateDisabled={updateDisabled}
               updateQty={updateBagQty}
-              updateTotals={updateTotals}
+              updateTotals={updateCartTotals}
             />
           )} 
           
@@ -207,12 +176,10 @@ const CodeCommerceApp = () => {
               <StatusBar progress={barProgress} />
               {page === "ship" && 
                 <Shipping
-                  errors={shipFormErrors}
-                  form={shipFormValues}
-                  method={shipMethod}
-                  checkFullForm={checkFullForm}
-                  updateMethod={updateShipMethod}
-                  updateVals={updateShipFormValues}
+                  disabled={disabled}
+                  updateDisabled={updateDisabled}
+                  updateInfo={updateShipInfo} 
+                  updateTotals={updateShipTotals}
                   goBack={changePageBack}
                 />
               }
@@ -227,13 +194,13 @@ const CodeCommerceApp = () => {
           }
           <Summary
             account={activeAccount}
-            addressInfo={shipFormValues}
+            // addressInfo={shipFormValues}
             bag={bag}
             discount={discount}
             page={page}
             itemTotals={items}
             shipCost={shipCost}
-            shipMethod={shipMethod}
+            // shipMethod={shipMethod}
             sub={subtotal}
             total={total}
             checkout={checkout}
